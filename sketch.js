839d86ebcc356e6a8279bd74e7ca09ac40b7d47c
Jeremy Southern <jezzd11@outlook.com>
2026-04-23 15:11:46 +0100

    Drag around functionality

--- sketch.js
+++ sketch.js
@@ -1,150 +1,92 @@
-const GRID_SIZE = 20;
-const CANVAS_SIZE = 600;
-const OFFSET = GRID_SIZE /2;
-let headX = [300];
-let headY = [300];
-let xDir = 0;
-let yDir = 0;
-let foodX;
-let foodY;
-let score = 0;
+// Hand Pose Detection with ml5.js
+// https://thecodingtrain.com/tracks/ml5js-beginners-guide/ml5/hand-pose
 
-function setup()
-{
-    createCanvas(CANVAS_SIZE,CANVAS_SIZE)
-    background(0);
-    createFood();
-    frameRate(3);
-    
-}
-function draw()
-{
-    background(0);
-    checkerBoard();
-    snakeLogic();
-    circleLogic();
-    boundaryCheck(headX,headY);   
-    console.log(headX[0],headY[0])
-}
-function boundaryCheck(x,y)
-{
-    //checks values of the snakes head against the canvas, ends game if out of bounds 
-    if (headX[0] >= CANVAS_SIZE|| headX[0] <=0 || headY[0] >= CANVAS_SIZE || headY[0]<=0 )
-    {
-        restartGame();
-    }
+let video;
+let handPose;
+let hands = [];
+let rectX;
+let rectY;
 
-}
-function createFood()
-{
-    //generates the food 
-    foodX = (round (random(0,CANVAS_SIZE)/GRID_SIZE) * GRID_SIZE + OFFSET)
-    foodY = (round (random(0,CANVAS_SIZE)/GRID_SIZE) * GRID_SIZE + OFFSET)
-}
-function keyPressed()
-{ 
-    if (key === 'w' || keyCode === UP_ARROW)
-    {        
-        if (!(yDir === 1))
-        {
-            xDir = 0;
-            yDir = -1;
-        }
-        //up
-    
-    }
-    if (key === 's' || keyCode === DOWN_ARROW)
-    {
-        if (!(yDir === -1))
-        {
-            xDir = 0;
-            yDir = 1;
-        }
-        //down
-    }
-    if (key === 'a' || keyCode === LEFT_ARROW)
-    {
-        if (!(xDir === 1))
-        {
-            xDir = -1;
-            yDir = 0;
-        }
-        //left
-        
-    }
-    if (key === 'd' || keyCode === RIGHT_ARROW)
-    {
-        if (!(xDir === -1))
-        {
-            xDir = 1;
-            yDir = 0;
-        }
-        //right
-    }
-    
-    
-}
-function snakeLogic()
-{
-    
-    for(let i = headX.length - 1; i > 0; i--)
-    {
-        headX[i] = headX[i-1];
-        headY[i] = headY[i-1];
-    }
 
-    headX[0] += GRID_SIZE * xDir;
-    headY[0] += GRID_SIZE * yDir;
-
-    for(let i =0; i<headX.length; i++)
-    {    
-        fill(0);
-        square(headX[i],headY[i],GRID_SIZE);        
-    }
-    
-}
-function circleLogic()
-{
-    circle(foodX, foodY, GRID_SIZE);
-    if ((headX[0] + OFFSET) === foodX && (headY[0] + OFFSET) === foodY)
-    {
-        createFood();
-        headX.push(headX[headX.length-1]+ GRID_SIZE)
-        headY.push(headY[headY.length-1])
-    }
+function preload() {
+  handPose = ml5.handPose({ flipped: true });
 }
 
-function checkerBoard()
+function mousePressed() {
+  //log(hands);
+}
+
+function gotHands(results) {
+  hands = results;
+}
+
+function setup() {
+  createCanvas(640, 480);
+  rectX = width/2
+    rectY = height/2
+  video = createCapture(VIDEO, { flipped: true });
+  video.hide();
+  handPose.detectStart(video, gotHands);
+}
+
+function draw() 
 {
-    noStroke();
-    let i =0;
-    for (let x = 0; x < CANVAS_SIZE; x += GRID_SIZE) 
-    {
-        i++;
-        for (let y = 0; y < CANVAS_SIZE; y += GRID_SIZE) 
+  background(0);
+  image(video, 0, 0);
+  fill(255)
+  rectMode(CENTER)
+  rect(rectX,rectY,50)
+
+  if (hands.length > 0) {
+    for (let hand of hands) {
+      if (hand.confidence > 0.1) {
+
+        // --- Pinch detection: thumb tip (4) + index tip (8) ---
+        let thumbTip = hand.keypoints[4];
+        let indexTip = hand.keypoints[8];
+        let pinchDist = dist(thumbTip.x, thumbTip.y, indexTip.x, indexTip.y);
+
+        if (pinchDist < 30) 
         {
-            i++;
-            if(i % 2 === 0)
+            let pointX = (indexTip.x + thumbTip.x)/2
+            let pointY = (indexTip.y + thumbTip.x)/2
+           fill(random(255),random(255),random(255))
+            if (abs(rectX - pointX) < 30 && abs(rectY - pointY) < 200) 
             {
-                fill (0,255,255)
-                square(x, y, GRID_SIZE);
+                rectX = pointX
+                rectY = pointY
             }
-            else
-            { 
-                fill(127,255,212);
-                square(x, y, GRID_SIZE);
-            }
-            
         }
+        // -----------------------------------------------------
+
+        for (let i = 0; i < hand.keypoints.length; i++) {
+          let keypoint = hand.keypoints[i];
+
+          if (hand.handedness == "Left") {
+            //fill(255, 0, 255);
+          } else {
+            //fill(255, 255, 0);
+          }
+
+          if (i > 0) {
+            strokeWeight(2);
+            let lastKeyPoint = hand.keypoints[i - 1];
+            line(keypoint.x, keypoint.y, lastKeyPoint.x, lastKeyPoint.y);
+          }
+
+          strokeWeight(0);
+          circle(keypoint.x, keypoint.y, 16);
+        }
+      }
     }
+  }
 }
-function restartGame()
+
+function keyPressed()
 {
-    gameOver = false;
-    headX = [300];
-    headY = [300];
-    xDir = 1;
-    yDir = 0;
-    score = 0;
-    createFood();
+    if (keyCode === 32)
+    {
+        rectX = width/2
+        rectY = height/2
+    }
 }
